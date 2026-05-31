390717d334144b66f771366be66c1c61575f41fa
Triumph <triumphaidenojie08@gmail.com>
2026-05-31 08:43:06 +0100

    feat: start collection page UI

--- src/routes/collections/$collectionId.tsx
+++ src/routes/collections/$collectionId.tsx
@@ -1,4 +1,7 @@
+import { Spinner } from "#/components/ui/spinner"
 import { getCollectionById } from "#/server/collections"
+import { Comment01Icon } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
 import { useQuery } from "@tanstack/react-query"
 import { createFileRoute, useParams } from "@tanstack/react-router"
 
@@ -8,17 +11,34 @@ export const Route = createFileRoute("/collections/$collectionId")({
 
 function RouteComponent() {
   const { collectionId } = useParams({ from: "/collections/$collectionId" })
-  const { data: collection = {}, isLoading: isLoadingCollection } = useQuery({
-    queryKey: `collection${collectionId}`,
-    queryFn: getCollectionById({ data: { collectionId } }),
+  const { data: collection, isLoading: isLoadingCollection } = useQuery({
+    queryKey: ["collection", collectionId],
+    queryFn: () => getCollectionById({ data: { collectionId } }),
   })
+
+  if (isLoadingCollection)
+    return (
+      <div className="flex-center h-dvh">
+        <Spinner />
+      </div>
+    )
+  if (!collection) return <div>Collection not found</div>
+
+  const {
+    _count: { comments: commentsCount, pages: pagesCount, saves: savesCount },
+  } = collection
 
   return (
     <section className="relative flex h-dvh flex-1 flex-col overflow-y-auto">
       <header></header>
       <main className="relative flex-1">{collectionId}</main>
       <nav className="bg-background sticky right-0 bottom-0 left-0 z-1000 flex h-max border-t md:hidden">
-        <ul className="flex-evenly h-12 px-4">y</ul>
+        <ul className="flex-evenly h-12 px-4">
+          <button className="flex items-center gap-1">
+            <HugeiconsIcon icon={Comment01Icon} />
+            {commentsCount}
+          </button>
+        </ul>
       </nav>
     </section>
   )
