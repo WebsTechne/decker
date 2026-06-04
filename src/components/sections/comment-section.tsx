94fa8f8e0ed5d3e130a400329b2ff98bafa4cff1
Triumph <triumphaidenojie08@gmail.com>
2026-06-04 11:16:52 +0100

    chore: refactor code and styling

--- src/components/sections/comment-section.tsx
+++ src/components/sections/comment-section.tsx
@@ -175,7 +175,7 @@ export function CommentsSheet({
         onOpenChange={onOpenChange}
         repositionInputs={false}
       >
-        <DrawerContent className="h-[90dvh] overflow-hidden has-[&_textarea:focus]:h-[50dvh]">
+        <DrawerContent className="h-[90dvh] overflow-hidden has-[textarea:focus]:h-[50dvh]">
           <DrawerHeader>
             <DrawerTitle className="text-lg">Comments</DrawerTitle>
           </DrawerHeader>
